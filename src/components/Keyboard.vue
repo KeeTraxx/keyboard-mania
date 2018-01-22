615f61f435eee8d6f9e875fd3df45d9c1b6f6666
Khôi Tran <tran@puzzle.ch>
2018-01-22 22:31:20 +0100

    midi importer

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,21 +1,25 @@
 <template>
   <div ref="container" @click="note(~~(Math.random() * 8))">
-
+    <input ref="fileinput" type="file">
   </div>
 </template>
 
 <script>
-
 import * as THREE from 'three'
 import * as TWEEN from '@tweenjs/tween.js'
+import * as Stats from 'stats.js'
+import * as MIDIParser from 'midi-parser-js/src/midi-parser.js'
+
+const stats = new Stats()
 THREE.Cache.enabled = true
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 50)
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 40)
+camera.position.x = 40
 camera.position.y = -55
-camera.lookAt(new THREE.Vector3(0, 0, 0))
+camera.position.z = 20
+camera.lookAt(new THREE.Vector3(40, 0, -30))
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x000000)
@@ -25,25 +29,26 @@ const dirLight = new THREE.DirectionalLight(0xffffff, 0.125)
 dirLight.position.set(0, 0, 1).normalize()
 scene.add(dirLight)
 
-const geometry = new THREE.SphereGeometry(5, 16, 16)
-const wireframeGreen = new THREE.MeshBasicMaterial({ color: 0x00cc00, wireframe: true })
-const sphere = new THREE.Mesh(geometry, wireframeGreen)
-
-scene.add(sphere)
+// const wireframeGreen = new THREE.MeshBasicMaterial({ color: 0x00cc00, wireframe: true })
 
 const basicGreen = new THREE.LineBasicMaterial({ color: 0x00cc00, linewidth: 2 })
 
-for (let i = 0; i < 8; i++) {
-  const group = new THREE.Group()
-  group.position.x = i * 1 - 4
+for (let octave = 4; octave < 8; octave++) {
+  for (let i = 0; i < 8; i++) {
+    const group = new THREE.Group()
+    group.position.x = octave * 8 + i * 1 - 4
 
-  // LANE
-  const box = new THREE.BoxGeometry(1, 100, 0.2)
-  const geo = new THREE.EdgesGeometry(box)
-  const lane = new THREE.LineSegments(geo, basicGreen)
-  group.add(lane)
-  scene.add(group)
+    // LANE
+    const box = new THREE.BoxGeometry(1, 100, 0.2)
+    const geo = new THREE.EdgesGeometry(box)
+    const lane = new THREE.LineSegments(geo, basicGreen)
+    group.add(lane)
+    scene.add(group)
+  }
 }
+
+let midiData
+let timeDivision
 
 export default {
   name: 'Keyboard',
@@ -54,6 +59,10 @@ export default {
     renderer.setSize(window.innerWidth, window.innerHeight)
     this.$refs.container.appendChild(renderer.domElement)
     this.animate()
+
+    this.$refs.container.appendChild(stats.dom)
+
+    MIDIParser.addListener(this.$refs.fileinput, midiData => this.loadMidiData(midiData))
   },
   methods: {
     onResize () {
@@ -67,23 +76,71 @@ export default {
       renderer.render(scene, camera)
     },
     animate (time) {
-      requestAnimationFrame(this.animate)
-      sphere.rotation.y += 0.01
-      sphere.rotation.z += 0.01
-      this.render()
-
+      stats.begin()
       TWEEN.update(time)
+      this.render()
+      stats.end()
+      requestAnimationFrame(this.animate)
     },
     note (note) {
-      console.log('Spawning...')
+      console.log(note)
+
+      let octave = ~~(note / 12)
+      let tone = note % 12
+
+      if (tone === 1) {
+        tone -= 0.5
+      } else if (tone > 1) {
+        tone -= 1
+      } else if (tone === 3) {
+        tone -= 1.5
+      } else if (tone > 3) {
+        tone -= 2
+      } else if (tone === 6) {
+        tone -= 2.5
+      } else if (tone > 6) {
+        tone -= 3
+      } else if (tone === 8) {
+        tone -= 3.5
+      } else if (tone > 8) {
+        tone -= 4
+      } else if (tone === 10) {
+        tone -= 4.5
+      } else {
+        tone -= 5
+      }
+
       let box = new THREE.BoxGeometry(1, 0.2, 0.2)
       let mesh = new THREE.Mesh(box, basicGreen)
-      mesh.position.x = note - 4
+      mesh.position.x = octave * 8 + tone
       scene.add(mesh)
       mesh.position.y = 0
       new TWEEN.Tween(mesh.position)
-        .to({ y: -100 }, 3000)
+        .to({ y: -100 }, 10000)
+        .onComplete(() => scene.remove(mesh))
         .start()
+    },
+    loadMidiData (data) {
+      console.log(data)
+      midiData = data.track[1].event
+      timeDivision = data.timeDivision
+      this.popMidi(0)
+    },
+    popMidi (index) {
+      // note ON
+      if (midiData[index].type === 9) {
+        this.note(midiData[index].data[0])
+      }
+
+      while (midiData[++index].deltaTime === 0) {
+        if (midiData[index].type === 9) {
+          this.note(midiData[index].data[0])
+        }
+      }
+
+      let deltaTime = midiData[index].deltaTime
+
+      setTimeout(() => this.popMidi(++index), deltaTime / timeDivision * 250)
     }
   }
 }
