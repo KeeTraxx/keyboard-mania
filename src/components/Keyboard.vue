fdea6ef2b8c0113ceb22cf60d5b201e37e84f238
Khôi Tran <kt@compile.ch>
2018-01-24 00:14:29 +0100

    wippler go

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div ref="container" @click="note(~~(Math.random() * 8))">
     <input ref="fileinput" type="file">
+    <div v-if="hasMIDISupport()">MIDI-Keyboard support only in Chrome</div>
   </div>
 </template>
 
@@ -9,8 +10,58 @@ import * as THREE from 'three'
 import * as TWEEN from '@tweenjs/tween.js'
 import * as Stats from 'stats.js'
 import * as MIDIParser from 'midi-parser-js/src/midi-parser.js'
+import * as MIDIInput from '../lib/MidiInput'
+
+function noteData (note) {
+  let octave = ~~(note / 12)
+  let tone = note % 12
+  let isHalfTone = false
+
+  if (tone === 0) {
+  } else if (tone === 1) {
+    isHalfTone = true
+    tone -= 0.5
+  } else if (tone < 3) {
+    tone -= 1
+  } else if (tone === 3) {
+    isHalfTone = true
+    tone -= 1.5
+  } else if (tone < 6) {
+    tone -= 2
+  } else if (tone === 6) {
+    isHalfTone = true
+    tone -= 2.5
+  } else if (tone < 8) {
+    tone -= 3
+  } else if (tone === 8) {
+    isHalfTone = true
+    tone -= 3.5
+  } else if (tone < 10) {
+    tone -= 4
+  } else if (tone === 10) {
+    isHalfTone = true
+    tone -= 4.5
+  } else if (tone < 13) {
+    tone -= 5
+  }
+
+  let position = octave * 7 + tone
+  return {
+    note,
+    octave,
+    tone,
+    position,
+    isHalfTone
+  }
+}
 
 const stats = new Stats()
+stats.domElement.style.position = 'absolute'
+stats.domElement.style.bottom = '5px'
+stats.domElement.style.left = '5px'
+stats.domElement.style.display = 'flex'
+stats.domElement.style.justifyContent = 'flex-end'
+stats.domElement.style.flexDirection = 'column'
 THREE.Cache.enabled = true
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -33,19 +84,43 @@ scene.add(dirLight)
 
 const basicGreen = new THREE.LineBasicMaterial({ color: 0x00cc00, linewidth: 2 })
 
-for (let octave = 4; octave < 8; octave++) {
-  for (let i = 0; i < 8; i++) {
-    const group = new THREE.Group()
-    group.position.x = octave * 8 + i * 1 - 4
+const basicBlue = new THREE.LineBasicMaterial({ color: 0x0000cc, linewidth: 2 })
 
+let lanes = []
+
+for (let octave = 4; octave < 8; octave++) {
+  const group = new THREE.Group()
+  group.position.x = octave * 7
+
+  for (let i = 0; i < 7; i++) {
     // LANE
     const box = new THREE.BoxGeometry(1, 100, 0.2)
     const geo = new THREE.EdgesGeometry(box)
     const lane = new THREE.LineSegments(geo, basicGreen)
+    lane.position.x = i
+    lanes[octave * 7 + i] = lane
     group.add(lane)
-    scene.add(group)
   }
+  scene.add(group)
 }
+
+MIDIInput.on('noteOn', ev => {
+  console.log(ev)
+  let note = noteData(ev.note)
+  console.log(note)
+  if (lanes[note.position]) {
+    let mesh = lanes[note.position]
+    mesh.material = basicBlue
+  }
+})
+
+MIDIInput.on('noteOff', ev => {
+  let note = noteData(ev.note)
+  if (lanes[note.position]) {
+    let mesh = lanes[note.position]
+    mesh.material = basicGreen
+  }
+})
 
 let midiData
 let timeDivision
@@ -82,65 +157,53 @@ export default {
       stats.end()
       requestAnimationFrame(this.animate)
     },
-    note (note) {
-      console.log(note)
-
-      let octave = ~~(note / 12)
-      let tone = note % 12
-
-      if (tone === 1) {
-        tone -= 0.5
-      } else if (tone > 1) {
-        tone -= 1
-      } else if (tone === 3) {
-        tone -= 1.5
-      } else if (tone > 3) {
-        tone -= 2
-      } else if (tone === 6) {
-        tone -= 2.5
-      } else if (tone > 6) {
-        tone -= 3
-      } else if (tone === 8) {
-        tone -= 3.5
-      } else if (tone > 8) {
-        tone -= 4
-      } else if (tone === 10) {
-        tone -= 4.5
-      } else {
-        tone -= 5
-      }
-
+    note (midiEvent) {
       let box = new THREE.BoxGeometry(1, 0.2, 0.2)
-      let mesh = new THREE.Mesh(box, basicGreen)
-      mesh.position.x = octave * 8 + tone
+      let mesh = new THREE.Mesh(box, midiEvent.isHalfTone ? basicGreen : basicBlue)
+      mesh.position.x = midiEvent.position
       scene.add(mesh)
       mesh.position.y = 0
       new TWEEN.Tween(mesh.position)
         .to({ y: -100 }, 10000)
-        .onComplete(() => scene.remove(mesh))
+        .onComplete(() => {
+          console.log('removing', mesh)
+          scene.remove(mesh)
+        })
         .start()
     },
     loadMidiData (data) {
       console.log(data)
-      midiData = data.track[1].event
+      midiData = data.track[1].event.map(e => {
+        if (e.type === 9 || e.type === 8) {
+          let dat = noteData(e.data)
+          e.octave = dat.octave
+          e.tone = dat.tone
+          e.position = dat.position
+        }
+
+        return e
+      })
       timeDivision = data.timeDivision
       this.popMidi(0)
     },
     popMidi (index) {
       // note ON
       if (midiData[index].type === 9) {
-        this.note(midiData[index].data[0])
+        this.note(midiData[index])
       }
 
       while (midiData[++index].deltaTime === 0) {
         if (midiData[index].type === 9) {
-          this.note(midiData[index].data[0])
+          this.note(midiData[index])
         }
       }
 
       let deltaTime = midiData[index].deltaTime
 
       setTimeout(() => this.popMidi(++index), deltaTime / timeDivision * 250)
+    },
+    hasMIDISupport () {
+      return navigator.hasOwnProperty('requestMIDIAccess')
     }
   }
 }
---
+++ src/lib/MidiInput.js
@@ -0,0 +1,67 @@
+
+const listeners = {}
+
+const inputs = {}
+
+navigator.requestMIDIAccess().then(midiAccess => {
+  midiAccess.inputs.forEach((port, key) => {
+    inputs[key] = port
+    addListeners(port)
+  })
+
+  midiAccess.onstatechange = function (e) {
+    switch (e.port.state) {
+      case 'disconnected':
+        delete inputs[e.port.id]
+        break
+      case 'connected':
+        inputs[e.port.id] = e.port
+        addListeners(e.port)
+        break
+    }
+  }
+})
+
+function toNote (event) {
+  return {
+    channel: event.data[0] & 0x0f,
+    type: event.data[0] >> 4,
+    velocity: event.data[2],
+    note: event.data[1],
+    timeStamp: event.timeStamp,
+    source: event.target
+  }
+}
+
+function addListeners (port) {
+  port.open().then(function () {
+    console.log('open', arguments)
+  })
+
+  port.onmidimessage = ev => {
+    switch (ev.data[0] & 0xf0) {
+      case 0x90:
+        if (listeners.hasOwnProperty('noteOn')) {
+          listeners.noteOn.forEach(cb => cb(toNote(ev)))
+        }
+        break
+      case 0x80:
+        if (listeners.hasOwnProperty('noteOff')) {
+          listeners.noteOff.forEach(cb => cb(toNote(ev)))
+        }
+        break
+    }
+  }
+}
+
+const on = (event, callback) => {
+  if (!listeners.hasOwnProperty(event)) {
+    listeners[event] = []
+  }
+
+  listeners[event].push(callback)
+}
+
+export {
+  on
+}
